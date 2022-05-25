4d51fe856a8447ea8da25797cc769ec28478a5a6
Itamar-Brito <itamar.brito@gmail.com>
2022-05-25 20:52:00 -0300

    ver status do pc

---
+++ stats.js
@@ -0,0 +1,26 @@
+const os= require('os')
+
+setInterval( () => {
+
+    const {freemem, totalmem} = os
+
+    const total = parseInt(totalmem() / 1024 / 1024)
+    const mem = parseInt(freemem()/ 1024 /1024)
+
+    const percent = parseInt( (mem / total) * 100)
+
+
+    const stats = {
+        free: `${mem} MB`,
+        total:`${total} MB`,
+        percent:`${percent} %`
+    } 
+
+    console.clear()
+    console.log("========== PC STATS ========== ")
+    console.table(stats)
+
+
+},1000)
+
+
